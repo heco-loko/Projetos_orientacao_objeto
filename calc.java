a96b90cd754139e96c95e0150d1a634183b6c739
Hector <hecos2mi@gmail.com>
2023-04-18 21:37:04 -0300

    calc.java praticando

---
+++ calc.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class calc {
+    static public void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            Numero n1 = new Numero();
+            Numero n2 = new Numero();
+            Numero res = new Numero();
+
+            System.out.printf("%nDigite o valor 1:");
+            n1.setValor(scan.nextInt());
+            System.out.printf("%nDigite o valor 2:");
+            n2.setValor(scan.nextInt());
+            res.setValor(n1.getValor() + n2.getValor());
+            System.out.printf("%nA soma de %d com %d e igual a %d", n1.getValor(), n2.getValor(), res.getValor());
+        }
+
+    }
+}
